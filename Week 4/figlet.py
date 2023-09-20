21098f8a58b72d037789ed82393863434b03660c
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-09-20 18:55:39 +0200

    add_brackets_to_elif

--- Week 4/figlet.py
+++ Week 4/figlet.py
@@ -8,7 +8,7 @@ font_list = figlet.getFonts()
 if len(sys.argv) == 1:
     random_font = random.choice(font_list)
     f = Figlet(font = random_font)
-elif len(sys.argv) == 3 and sys.argv[2] in font_list and sys.argv[1] in ["-f", "--font"]:
+elif len(sys.argv) == 3 and sys.argv[2] in font_list and (sys.argv[1] in ["-f","--font"]):
     f = Figlet(font = sys.argv[2])
 else:
     sys.exit("Invalid usage")
