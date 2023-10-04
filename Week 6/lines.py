c517256f68edfde7d42435ea2011113fc73e6027
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-10-04 15:10:34 +0200

    add_functions_and_correct_line_counting

--- Week 6/lines.py
+++ Week 6/lines.py
@@ -1,19 +1,29 @@
 import sys
 
-try:
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    if len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
+def main():
+    check_command_line_arguments()
+    count_lines()
 
-    name,extension = sys.argv[1].split('.')
-    if extension != "py":
-        sys.exit("Not a Python file")
-    else:
-        with open(sys.argv[1], "r") as file:
-            lines_num = 0
-            for line in file:
+
+def check_command_line_arguments():
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        if len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        if not sys.argv[1].endswith(".py"):
+            sys.exit("Not a Python file")
+    except FileNotFoundError:
+        print("File does not exist")
+
+def count_lines():
+    with open(sys.argv[1], "r") as file:
+        lines_num = 0
+        # loop through the lines to check if starts with # or whitespace
+        for line in file:
+            if not (line.lstrip().startswith("#") or line.isspace()):
                 lines_num += 1
-            print(lines_num)
-except FileNotFoundError:
-    print("File does not exist")
+    print(lines_num)
+
+if __name__ == "__main__":
+    main()
