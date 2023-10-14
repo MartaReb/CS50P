7f4af8c1cba3182668e2d08ed88d5d55433d5220
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-10-14 18:40:38 +0200

    add_print_pizza_table_function

--- Week 6/pizza/pizza.py
+++ Week 6/pizza/pizza.py
@@ -1,17 +1,27 @@
 import sys
+import csv
+from tabulate import tabulate
 
 def main():
     check_command_line_arguments()
-
+    print_pizza_table()
 
 def check_command_line_arguments():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    if not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+def print_pizza_table():
+    pizza_table = []
     try:
-        if len(sys.argv) < 2:
-            sys.exit("Too few command-line arguments")
-        if len(sys.argv) > 2:
-            sys.exit("Too many command-line arguments")
-        if not sys.argv[1].endswith(".csv"):
-            sys.exit("Not a CSV file")
+        with open(sys.argv[1]) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                pizza_table.append(row)
+            print(tabulate(pizza_table, headers="keys", tablefmt="grid"))
     except FileNotFoundError:
         sys.exit("File does not exist")
 
