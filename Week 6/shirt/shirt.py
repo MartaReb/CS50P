505701de73e0725f46cd26899c668b1f78c1168e
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-11-12 18:26:18 +0100

    add_create_a_new_image_function

--- Week 6/shirt/shirt.py
+++ Week 6/shirt/shirt.py
@@ -1,7 +1,9 @@
 import sys
+from PIL import Image, ImageOps
 
 def main():
     check_command_line_arguments()
+    create_a_new_image()
 
 
 def check_command_line_arguments():
@@ -16,6 +18,18 @@ def check_command_line_arguments():
             sys.exit("Invalid output")
         elif not (extension1 == extension2):
             sys.exit("Input and output have different extensions")
+
+def create_a_new_image():
+    try:
+        img = Image.open(sys.argv[1])
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+
+    shirt = Image.open("shirt.png")
+    size = shirt.size
+    img_new_size = ImageOps.fit(img, size)
+    img_new_size.paste(shirt, shirt)
+    img_new_size.save(sys.argv[2])
 
 if __name__ == "__main__":
     main()
