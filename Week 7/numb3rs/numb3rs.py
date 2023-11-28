be65370557d59a5bf9463280c8fbed38a94224d8
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-11-28 18:22:46 +0100

    Add a check to verify whether the IPv4 number consists of correct numbers

--- Week 7/numb3rs/numb3rs.py
+++ Week 7/numb3rs/numb3rs.py
@@ -5,11 +5,15 @@ def main():
 
 
 def validate(ip):
-    if re.search(r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip):
-        print("True")
+    ip_number = re.search(r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip)
+    if ip_number:
+        for number in ip_number.groups():
+            if int(number) > 255:
+                return False
+        else:
+            return True
     else:
-        print("False")
-
+        return False
 
 if __name__ == "__main__":
     main()
