1f10f9c83666e2c1eddbe9ea144ee0d34e477c0e
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-12-09 19:57:41 +0100

    add_convert_hour_and_convert_minutes_functions

--- Week 7/working/working.py
+++ Week 7/working/working.py
@@ -1,34 +1,40 @@
 import re
 
 def main():
-    print(convert(input("Hours: ")))
+        print(convert(input("Hours: ")))
 
 def convert(s):
-    time = re.search(r"^([0-9]+):?([0-9]*)?\s(AM|PM)\sto\s([0-9]+):?([0-9]*)?\s(AM|PM)$",s)
-    if int(time.group(1)) < 13 or int(time.group(4)) < 13:
-        if time.group(3) == "AM" and int(time.group(1)) == 12:
-            new_hour1 = "00"
-        else:
-            new_hour1 = int(time.group(1))
-        if time.group(6) == "AM" and int(time.group(4)) == 12:
-            new_hour2 = "00"
-        else:
-            new_hour2 = int(time.group(4))
-        if time.group(3) == "PM" and int(time.group(1)) != 12:
-            new_hour1 = int(time.group(1)) + 12
-        if time.group(6) == "PM" and int(time.group(4)) != 12:
-            new_hour2 = int(time.group(4)) + 12
-        if time.group(2) == "":
-            new_minutes1 = "00"
-        if time.group(5) == "":
-            new_minutes2 = "00"
-        elif int(time.group(2)) < 60 or int(time.group(5)) < 60:
-            new_minutes1 = time.group(2)
-            new_minutes2 = time.group(5)
-        else:
-            raise ValueError
+    regex = "([0-9]+):?([0-9]*)? (AM|PM)"
+    time = re.search(r"^" + regex + " to " + regex + "$",s)
+
+    if time:
+        new_hour1 = convert_hour(time.group(1), time.group(3))
+        new_hour2 = convert_hour(time.group(4), time.group(6))
+        new_minutes1 = convert_minutes(time.group(2))
+        new_minutes2 = convert_minutes(time.group(5))
         return f"{new_hour1:02}:{new_minutes1} to {new_hour2:02}:{new_minutes2}"
     else:
         raise ValueError
+
+def convert_hour(hour, period):
+    hour = int(hour)
+    if hour < 13:
+        if period == "AM" and hour == 12:
+            return 0
+        elif period == "PM" and hour != 12:
+            return hour + 12
+        else:
+            return hour
+    else:
+        raise ValueError
+
+def convert_minutes(minutes):
+    if minutes == "":
+        return "00"
+    elif int(minutes) < 60:
+        return minutes
+    else:
+        raise ValueError
+
 if __name__ == "__main__":
     main()
