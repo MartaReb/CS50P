e67db164b81e5691eeb3d528240f11fce6494fa0
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-12-19 14:47:06 +0100

    add_andword_parameter_and_change_pattern

--- Week 8/seasons/seasons.py
+++ Week 8/seasons/seasons.py
@@ -9,14 +9,14 @@ def main():
     print(convert(input("Date of birth: ")))
 
 def convert(d):
-    time = re.search("^([1-2][0-9][0-9][0-9])-([0-1][0-9])-([0-9][0-9])$", d)
+    time = re.search("^([1-2][0-9]{3})-([0-1][0-9])-([0-3][0-9])$", d)
     if time:
         year, month, day = time.groups()
         date_of_birth = date(int(year), int(month), int(day))
         current_date = date.today()
         time_difference = current_date - date_of_birth
         seconds_difference = int(time_difference.total_seconds()/60)
-        words = (p.number_to_words(seconds_difference)).capitalize()
+        words = (p.number_to_words(seconds_difference, andword="")).capitalize()
         return f"{words} minutes"
     else:
         sys.exit("Invalid date")
