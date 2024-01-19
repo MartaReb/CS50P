c0f353ea3a3db4c92c7e4ddfe03ee68067174e25
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-01-19 14:24:30 +0100

    add functions: get_data_from_api, user_answer; change function: country_and_capital_list on country_list andcapital_list

--- final_project/project.py
+++ final_project/project.py
@@ -2,25 +2,56 @@ import requests
 import random
 
 def main():
+    print("Try to guess all the countries' capitals. You have 5 chances.")
+    body = get_data_from_api()
+    number = 5 
+    score = 0
+    final_score = 0
+    while number > 0:
+        country_num = country_list(body)
+        capital_name = capital_list(body, country_num)
+        print("What is the capital? ")
+        answer = input().strip()
+        outcome = user_answer(answer, capital_name, score)
+        if outcome == 1:
+            final_score += 1
+        number = number - 1
+    
+    print(f"Your final score is: {final_score}/5")
+
+def get_data_from_api():
     url = "https://restcountries.com/v3.1/all"
     response = requests.get(url)
-    body = response.json()
-    capital_name = country_and_capital_list(body)
-
-    print("What is the capital?")
-    answer = input()
-    if answer == capital_name:
-        print("Yeah! That's right!")
+    if response.status_code == 200:
+        body = response.json()
+        return body
     else:
-        print("Try again")
-        print(capital_name)
+        print(f"Error while downloading data. Error code: {response.status_code}")
 
-def country_and_capital_list(body):
-    country = [c['name']['official'] for c in body]
-    capital = [c.get('capital') for c in body]
+def country_list(body):
+    country = []
+    for c in body:
+        if c.get("independent") is True:
+            country.append(c["name"]["official"])
     country_num = random.randint(0, (len(country)))
-    print(country[country_num])
+    print(f"Country: {country[country_num]}")
+    return country_num
+
+def capital_list(body, country_num):
+    capital = []
+    for c in body:
+        if c.get("independent") is True:
+            capital.append(c.get("capital"))
     return str(capital[country_num])[2:-2]
+
+def user_answer(answer, capital_name, score):
+    if answer == capital_name:
+        score += 1
+        print(f"Yeah! That's right!")
+    else:
+        print(f"The right answer is {capital_name}.Try again!")
+    print()
+    return score
 
 if __name__ == "__main__":
     main()
